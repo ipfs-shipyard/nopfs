1731002b540e0334cd8c97ae2c3b9c4e6a72337b
Hector Sanjuan <code@hector.link>
2023-03-27 01:52:35 +0200

    Add testing CLI

---
+++ cmd/nopfs/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/hsanjuan/nopfs"
+	"github.com/ipfs/go-cid"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/ipfs/go-path"
+)
+
+func printPrompt() {
+	fmt.Print("> ")
+}
+
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("> c <cid>")
+	fmt.Println("> p <path>")
+}
+
+func main() {
+	logging.SetLogLevel("nopfs", "DEBUG")
+	filename := "test.deny"
+	if len(os.Args) < 2 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println("nopfs: denylist testing REPL")
+		fmt.Println()
+		fmt.Println("Usage: ./nopfs list.deny")
+		return
+	}
+	filename = os.Args[1]
+	blocker, err := nopfs.NewBlocker([]string{filename})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT)
+
+	reader := bufio.NewScanner(os.Stdin)
+	printUsage()
+	fmt.Println("[CTRL-D to exit]")
+	printPrompt()
+	for reader.Scan() {
+		select {
+		case <-sigCh:
+			fmt.Println(blocker.Close())
+			return
+		default:
+		}
+
+		text := reader.Text()
+		typ, elem, found := strings.Cut(text, " ")
+		if !found {
+			fmt.Println("not found")
+			printPrompt()
+			continue
+		}
+		switch typ {
+		case "p":
+			status := blocker.IsPathBlocked(path.FromString(elem))
+			fmt.Printf("%s: %s\n", status.Status, status.Entry)
+		case "c":
+			c, err := cid.Decode(elem)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				status := blocker.IsCidBlocked(c)
+				fmt.Printf("%s: line %d. %s\n", status.Status, status.Entry.Line, status.Entry)
+			}
+		default:
+			fmt.Println("Usage:")
+			fmt.Println("> c <cid>")
+			fmt.Println("> p <path>")
+		}
+		printPrompt()
+
+	}
+	fmt.Println()
+	fmt.Println(blocker.Close())
+}
