018cf4ad2223dd98ebf3c0be24bb9af3ef4ed164
Hector Sanjuan <code@hector.link>
2023-10-03 14:22:29 +0200

    util: s/Folder/Directory. Do GetDenylistFilesinDir()

--- util.go
+++ util.go
@@ -9,8 +9,8 @@ import (
 )
 
 // GetDenylistFiles returns a list of ".deny" files found in
-// $XDG_CONFIG_HOME/ipfs/denylists and /etc/ipfs/denylists.  The files are
-// sortered by their names in their respective folders.
+// $XDG_CONFIG_HOME/ipfs/denylists and /etc/ipfs/denylists. The files are
+// sortered by their names in their respective directories.
 func GetDenylistFiles() ([]string, error) {
 	// First, look for denylists in $XDG_CONFIG_HOME/ipfs/denylists
 	xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
@@ -18,14 +18,14 @@ func GetDenylistFiles() ([]string, error) {
 		xdgConfigHome = os.Getenv("HOME") + "/.config"
 	}
 	ipfsDenylistPath := filepath.Join(xdgConfigHome, "ipfs", "denylists")
-	ipfsDenylistFiles, err := GetDenylistFilesInFolder(ipfsDenylistPath)
+	ipfsDenylistFiles, err := GetDenylistFilesInDir(ipfsDenylistPath)
 	if err != nil {
 		return nil, err
 	}
 
 	// Then, look for denylists in /etc/ipfs/denylists
 	etcDenylistPath := "/etc/ipfs/denylists"
-	etcDenylistFiles, err := GetDenylistFilesInFolder(etcDenylistPath)
+	etcDenylistFiles, err := GetDenylistFilesInDir(etcDenylistPath)
 	if err != nil {
 		return nil, err
 	}
@@ -33,10 +33,10 @@ func GetDenylistFiles() ([]string, error) {
 	return append(ipfsDenylistFiles, etcDenylistFiles...), nil
 }
 
-// GetDenylistFilesInFolder returns a list of ".deny" files found in the given
-// folder. The files are sortered by their names. It returns an empty list and
-// no error if the folder does not exist.
-func GetDenylistFilesInFolder(dirpath string) ([]string, error) {
+// GetDenylistFilesInDir returns a list of ".deny" files found in the given
+// directory. The files are sortered by their names. It returns an empty list
+// and no error if the directory does not exist.
+func GetDenylistFilesInDir(dirpath string) ([]string, error) {
 	var denylistFiles []string
 
 	// WalkDir outputs files in lexical order.
